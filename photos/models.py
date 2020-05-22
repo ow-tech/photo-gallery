a91b47d4e81358dd410f02f6ec37a6aa07e29bbd
ow-tech <wagwanwekon@outloo.com>
2020-05-22 15:00:07 +0300

    creation of Image, location and category models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    imange_name = models.CharField(max_length=20)
+    image_Descrption = models.TextField()
+    # imange =
+    location = models.ForeignKey('Location', on_delete=models.DO_NOTHING)
+    category = models.ForeignKey('Category', on_delete=models.DO_NOTHING)
+
+    def __str__ (self):
+        return self.imange_name
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=30)
+
+
+    def __str__ (self):
+        return self.imange_name
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+
+
+
+    def __str__ (self):
+        return self.imange_name
