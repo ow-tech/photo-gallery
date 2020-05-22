fdee466a8ed2e90f0adeafb999bbfcf9645f292f
ow-tech <wagwanwekon@outloo.com>
2020-05-22 22:31:26 +0300

    modifications

--- photos/urls.py
+++ photos/urls.py
@@ -7,5 +7,5 @@ urlpatterns=[
     path('', views.my_images, name ='main')
 ]
 
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
