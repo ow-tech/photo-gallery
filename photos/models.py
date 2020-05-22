c18c913891a24d20b72f2f4d5248ad9f977c7374
ow-tech <wagwanwekon@outloo.com>
2020-05-22 15:51:13 +0300

    test for setup and save of image

--- photos/models.py
+++ photos/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Image(models.Model):
-    imange_name = models.CharField(max_length=20)
-    image_Descrption = models.TextField()
+    image_name = models.CharField(max_length=20)
+    image_descrption = models.TextField()
     # imange =
     location = models.ForeignKey('Location', on_delete=models.DO_NOTHING)
     category = models.ForeignKey('Category', on_delete=models.DO_NOTHING)
+    # pub_date = models.DateTimeField(auto_now_add=True, default=date.today)
 
     def __str__ (self):
         return self.imange_name
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Image, Location, Category
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+
+    def setUp(self):
+        self.expression= Image(image_name ='expression', image_descrption ='This is an image that shows my expression',)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.expression,Image))
+
+    def test_save_method(self):
+        self.expression.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)> 0)
