866088845b018de06eb7b61383da8efe6a131f39
ow-tech <wagwanwekon@outloo.com>
2020-05-23 13:49:39 +0300

    creation of view for the searched images

--- photos/views.py
+++ photos/views.py
@@ -4,3 +4,14 @@ from .models import Image
 def my_images (request):
     images = Image.all_images()
     return render(request, "main.html",{"images":images})
+
+def search_results(request):
+    if 'image' in request.GET and request.Get["image"]:
+        search_category = request.GET.get("article")
+        searched_images = Image.search_by_category(search_category)
+        message = f"{search_category}"
+
+        return render(request, 'search.html',{"message":message, "images":searched_images})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
