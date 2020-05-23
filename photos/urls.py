7b87d171205b7790e5baf98c8ba501de8b4580b2
ow-tech <wagwanwekon@outloo.com>
2020-05-23 13:35:19 +0300

    creation of search form

--- photos/urls.py
+++ photos/urls.py
@@ -6,6 +6,5 @@ from django.conf.urls.static import static
 urlpatterns=[
     path('', views.my_images, name ='main')
 ]
-
-if settings.DEBUG is True:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
